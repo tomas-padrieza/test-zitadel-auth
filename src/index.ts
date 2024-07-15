86f390a5543d75df9d4e744a84f1de2ca5413ec7
Tomas Padrieza <tomas.padrieza@gmail.com>
2024-07-15 16:19:53 +0300

    wip: more realistic example with perm check

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,63 @@ import { bearerAuth } from 'hono/bearer-auth';
 
 const app = new Hono();
 
+const users: Record<string, any> = {
+    '276071138566602755': {
+        permissions: ['user'],
+    },
+    '01j2v273vq5wf4yhnw5sgb97sv': {
+        permissions: ['user', 'admin'],
+    },
+};
+
+async function verifyToken(token: string, tenantId: string, permission: string) {
+    const response = await fetch(`http://${tenantId}.127.0.0.1.sslip.io/auth/v1/users/me`, {
+        method: 'get',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const data = await response.json();
+
+    return users[data.user.id].permissions.indexOf(permission) !== -1;
+}
+
 app.use(
-    '/*',
+    '/admin/*',
     bearerAuth({
         verifyToken: async (token, c) => {
-            const responce = await fetch('http://acme.127.0.0.1.sslip.io/auth/v1/users/me', {
-                method: 'get',
-                headers: {
-                    Accept: 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+            // TODO: typebox transforms/checks
+            const { payload } = await c.req.json();
 
-            return responce.status === 200;
+            return verifyToken(token, payload.tenantId, 'admin');
         },
     })
 );
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!');
+app.use(
+    '/user/*',
+    bearerAuth({
+        verifyToken: async (token, c) => {
+            // TODO: typebox transforms/checks
+            const { payload } = await c.req.json();
+
+            return verifyToken(token, payload.tenantId, 'user');
+        },
+    })
+);
+
+app.post('/user', (c) => {
+    return c.text('Hello User!');
+});
+
+app.post('/admin', (c) => {
+    return c.text('Hello Admin!');
 });
 
 const port = 3000;
